9144f4a8d1f4267ac5f9edfadd80306aa97a23a2
ErfanMelon <ErfanMelon@outlook.com>
2024-11-14 14:29:54 +0330

    feat : add login/register to app

---
+++ models/RegisterForm.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+use yii\helpers\VarDumper;
+
+class RegisterForm extends Model
+{
+    public $user_name;
+    public $password;
+    public $email;
+    public $password_repeat;
+
+    public function rules()
+    {
+        return [
+            [['user_name', 'password', 'email', 'password_repeat'], 'required'],
+            ['email', 'email'],
+            ['user_name', 'string', 'min' => 3, 'max' => 150],
+            [['password', 'password_repeat'], 'string', 'min' => 8, 'max' => 255],
+            ['password_repeat', 'compare', 'compareAttribute' => 'password'],
+        ];
+    }
+
+    public function register()
+    {
+        $user = new User();
+        $user->user_name = $this->user_name;
+        $user->email = $this->email;
+        $user->password = \Yii::$app->security->generatePasswordHash($this->password);
+        $user->access_token = \Yii::$app->security->generateRandomString();
+        $user->auth_key = \Yii::$app->security->generateRandomString();
+
+        if($user->save()){
+            return true;
+        }
+        \Yii::error('user not registered '.VarDumper::dumpAsString($user->errors));
+        return false;
+    }
+}
